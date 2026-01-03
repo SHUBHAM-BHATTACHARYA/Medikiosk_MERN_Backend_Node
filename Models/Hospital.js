ad675bc023af3ec5092fc739d866adc05d87caf3
SHUBHAM-BHATTACHARYA <60126207+SHUBHAM-BHATTACHARYA@users.noreply.github.com>
2026-01-03 23:20:39 +0530

    updated the Hospital Model

--- Models/Hospital.js
+++ Models/Hospital.js
@@ -1,54 +1,45 @@
 const mongoose = require('mongoose')
-const Address = require('../Models/Address')
 
 const collectionName = 'Hospital-Collection'
 
-const hospitalSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true
-    },
-    type:{
-        type: String,
-        required: true
-    },
-    capacity:{
-        type: Number,
-        required: true
-    },
-    contactInfo:{
-        phone: {
-            type: Number,
-            required: true
-        },
-        email:{
-            type: String,
-            required: true,
-            index: {
-                unique: true
-            }
-        },
-        address: [Address]
-    },
-    createdBy: {
-        type: String,
-        required: true
-    },
-    modifiedBy: {
-        type: String,
-        required: true
-    },
-    createTimeStamp: {
-        type: Date,
-        required: true,
-        'default': Date.now
-    },
-    modifyTimeStamp: {
-        type: Date,
-        required: true,
-        'default': Date.now
-    }
+const contactInfoSchema = new mongoose.Schema({
+    phone: { type: Number, required: true },
+    email: { type: String, required: true }
+}, {_id: false })
 
+const locationSchema = new mongoose.Schema({
+    place: { type: String, required: true },
+    city: { type: String, required: true }
+}, {_id: false })
+
+const timingsSchema = new mongoose.Schema({
+    days: { type: String, required: true },
+    time: { type: String, required: true }
+}, {_id: false })
+
+const availableDoctorSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    experience: { type: Number, required: true },
+    fees: { type: Number, required: true },
+    timings: [ timingsSchema ]
+}, {_id: false })
+
+const specialitySchema = new mongoose.Schema({
+    name:{ type: String, required: true },
+    availableDoctor: [ availableDoctorSchema ]
+}, {_id: false })
+
+const hospitalSchema = new mongoose.Schema({
+    name:{ type: String, required: true },
+    type:{ type: String, required: true },
+    capacity:{ type: Number, required: true },
+    Timings: { type: String, required: true },
+    contactInfo: contactInfoSchema,
+    Location: [ locationSchema ],
+    procedure: { type: String, required: true },
+    speciality: [ specialitySchema ],
+    createTimeStamp: { type: Date, required: true, default: Date.now },
+    modifyTimeStamp: { type: Date, required: true, default:Date.now}
 })
 
 module.exports = mongoose.model('Hospital', hospitalSchema, collectionName)
--- routes/deleteRequests.js
+++ routes/deleteRequests.js
@@ -2,10 +2,13 @@ const express = require('express')
 const router = express.Router()
 const { deleteMedicineById } = require('../Controller/Medicines')
 const { deleteHospitalById } = require('../Controller/Hospitals')
+const { deleteClinicById } = require('../Controller/Clinics')
 
 //Delete Medicine by Id
 router.delete('/medicines/deleteMedicineById/:id', deleteMedicineById)
 
 router.delete('/hospitals/deleteHospitalById/:id', deleteHospitalById)
+
+router.delete('./clinics/deleteClinicById/:id', deleteClinicById)
 
 module.exports = router
